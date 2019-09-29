1546821437f733c183e0bf800c86cb45228a6447
qurinastha <qshrestha@gmail.com>
2019-09-29 15:42:49 +0545

    updated json file and rename slug name of theme demo

--- easy-store/demo-config.php
+++ easy-store/demo-config.php
@@ -20,16 +20,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 
     $theme_slug 	= 'easy-store';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_description.'/';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
 	$demo_config 	= array(
 		'default' => array(
 			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
 			'theme_description' => esc_html__( 'Easy Ecommerce WordPress Theme', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $giturl . 'default/screenshot.png',
+			'xml_file'     		=> $git_url . $theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'screenshot.png',
 			'preview_url'		=> 'http://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> true,
 			'theme_type'        => array( 'free', 'WooCommerce', 'Ecommerce' ),
