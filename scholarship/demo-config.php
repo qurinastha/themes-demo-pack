3b94121a52cbf2e5135d64fcb8e959fd02632bc9
qurinastha <qshrestha@gmail.com>
2020-01-07 23:23:16 +0545

    scholarship json file changes

--- scholarship/demo-config.php
+++ scholarship/demo-config.php
@@ -21,28 +21,28 @@ function mt_demo_importer_config(){
 	$theme_slug 	= 'scholarship';
 	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
 	$demo_config 	= array(
-		'default' => array(
-			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
+		'scholarship' => array(
+			'name'				=> esc_html__( 'Scholarship', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
 			'theme_description' => esc_html__( 'Best Free WordPress Education Theme.', 'mystery-demo-importer' ),
 			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
 			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
 			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'default/screenshot.jpg',
+			'preview_screen'	=> $git_url . 'default/screenshot.png',
 			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
+			'theme_type'        => array( 'free', 'education', 'portfolio' ),
+			'show_on_front'     => 'page',
 			'home_title'  		=> 'Home',
-			'blog_title'  		=> '',
+			'blog_title'  		=> 'Blog',
 			'menus_locations'	=> array(
-									'scholarship_primary_menu' => 'Primary Menu',
-								),
-			'required_plugins'  => array(
-				'free' => array(
-					array(
-						'plugin_name'  	=> esc_html__( 'Contact Form 7', 'mystery-demo-importer' ),
-						'plugin_slug'  	=> 'contact-form-7',
-						'install_setup' => 'contact-form-7/wp-contact-form-7.php'
-					)
+				'scholarship_primary_menu' => 'Primary Menu',
+			),
+			'plugins_list' => array(
+				'contact-form-7' => array(
+					'name'		=> __( 'Contact Form 7', 'mystery-demo-importer' ),
+					'slug'		=> 'contact-form-7/wp-contact-form-7.php',
+					'required'	=> true
 				)
 			)
 		)
